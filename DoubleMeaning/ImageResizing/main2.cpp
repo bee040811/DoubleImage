4b4b8fe7a50129926a2492e007b0d8d4e056c072
許小蜜 <bee040811@hotmail.com>
2014-02-25 15:51:16 +0800

    add main2 file

---
+++ DoubleMeaning/ImageResizing/main2.cpp
@@ -0,0 +1,42 @@
+#include "main.h"
+
+void manager_image::saveSelectedMeshFile(string filename) {
+	// save selected mesh 
+	// need to save into XML file 
+	// by map iterator method to save combine way & node 
+	// write xml file 
+	map<int, map<int,unsigned long long int>>::iterator it;
+	map<int,unsigned long long int>::iterator it_inner;
+
+	System::Xml::XmlDocument^ file = gcnew XmlDocument();
+	file->LoadXml("<mesh></mesh>");
+
+	for(int i = 0; i < FaceSelect.size() ; i++) {
+		if(FaceSelect[i] == true) {
+			XmlElement^ elem = file->CreateElement( "nodes" );
+			elem->SetAttribute("id",i.ToString());
+			file->DocumentElement->AppendChild( elem );
+		}
+	}
+
+	System::String^ filenameRef = gcnew  System::String(filename.c_str());
+	file->Save(filenameRef);
+}
+
+void manager_image::loadSelectedMeshFile(string filename) {
+	// save selected mesh 
+	// need to save into XML file
+	map<int, map<int,unsigned long long int>>::iterator it;
+	map<int,unsigned long long int>::iterator it_inner;
+	System::Xml::XmlDocument^ file = gcnew XmlDocument();
+	file->Load( gcnew System::String( filename.c_str()) );
+	XmlNode^ root = file->DocumentElement;
+	XmlNodeList^ nodeList = root->SelectNodes("nodes");
+	System::Collections::IEnumerator^ nodeListEnum = nodeList->GetEnumerator();
+	while(nodeListEnum->MoveNext()) {
+		XmlNode^ perNode;
+		perNode = safe_cast<XmlNode^>(nodeListEnum->Current);
+		unsigned long long int id = Convert::ToInt32(perNode->Attributes->GetNamedItem("id")->Value);
+		FaceSelect[id] = true;
+	}
+}
